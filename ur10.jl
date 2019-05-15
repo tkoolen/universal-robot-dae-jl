69cad4c5b03149c6c220c757886d6d8b84d23b7b
Twan Koolen <koolen.twan@gmail.com>
2019-05-15 01:01:44 -0400

    Use experimental stl/dae support.

--- ur10.jl
+++ ur10.jl
@@ -1,10 +1,20 @@
-
 using Pkg
 Pkg.activate(@__DIR__)
 
-using RigidBodyDynamics, MeshCatMechanisms
+using RigidBodyDynamics, MeshCatMechanisms, Random
 
 urdf = joinpath(@__DIR__, "ur10.urdf")
-robot = parse_urdf(urdf)
-mvis = MechanismVisualizer(robot, URDFVisuals(urdf))
+robot = parse_urdf(urdf, remove_fixed_tree_joints=false)
+state = MechanismState(robot)
+rand!(state)
+visuals = URDFVisuals(urdf; tag="collision") # works
+# visuals = URDFVisuals(urdf) # only displays base
+mvis = MechanismVisualizer(state, visuals)
+
+elements = visual_elements(robot, visuals)
+
 open(mvis)
+
+using Blink
+window = Window()
+body!(window, manipulate!(mvis))
